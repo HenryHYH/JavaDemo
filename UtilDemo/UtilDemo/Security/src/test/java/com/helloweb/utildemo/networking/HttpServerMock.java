3456b93b5f6a432ce29c1b28afafa562075aedeb
Henry <henry.hyh@gmail.com>
2017-07-25 10:28:04 +0800

    UtilDemo，使用jetty mock http

---
+++ UtilDemo/UtilDemo/Security/src/test/java/com/helloweb/utildemo/networking/HttpServerMock.java
@@ -0,0 +1,91 @@
+package com.helloweb.utildemo.networking;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+public class HttpServerMock {
+
+	public final static int DEFAULT_PORT = 10000;
+	public final static String DEFAULT_CONTENT_TYPE = "application/json";
+	public final static int DEFAULT_STATUS_CODE = HttpServletResponse.SC_OK;
+
+	private Server _httpServer;
+	private int _port;
+
+	public HttpServerMock() {
+		_port = DEFAULT_PORT;
+	}
+
+	public HttpServerMock(int port) {
+		_port = port;
+	}
+
+	/**
+	 * 启动Jetty服务器。默认的响应status code为"200"，content type为"application/json"。
+	 * 
+	 * @param content
+	 *            响应内容
+	 */
+	public void start(String content) throws Exception {
+		start(content, DEFAULT_CONTENT_TYPE, DEFAULT_STATUS_CODE);
+	}
+
+	/**
+	 * 启动Jetty服务器。默认的响应status code为"200"。
+	 * 
+	 * @param content
+	 *            响应内容
+	 * @param contentType
+	 *            响应内容的MIME类型
+	 */
+	public void start(String content, String contentType) throws Exception {
+		start(content, contentType, DEFAULT_STATUS_CODE);
+	}
+
+	/**
+	 * 启动Jetty服务器。
+	 * 
+	 * @param content
+	 *            响应内容
+	 * @param contentType
+	 *            响应内容的MIME类型
+	 * @param statuCode
+	 *            响应状态码
+	 */
+	public void start(String content, String contentType, int statuCode) throws Exception {
+		_httpServer = new Server(_port);
+		_httpServer.setHandler(createHandler(content, contentType, statuCode));
+		_httpServer.start();
+	}
+
+	/**
+	 * 停止Jetty服务器。
+	 */
+	public void stop() throws Exception {
+		if (null != _httpServer) {
+			_httpServer.stop();
+			_httpServer = null;
+		}
+	}
+
+	private Handler createHandler(final String content, final String contentType, final int statusCode) {
+		return new AbstractHandler() {
+			@Override
+			public void handle(String target, Request baseRequest, HttpServletRequest request,
+					HttpServletResponse response) throws IOException, ServletException {
+				response.setContentType(contentType);
+				response.setStatus(statusCode);
+				baseRequest.setHandled(true);
+				response.getWriter().print(content);
+			}
+		};
+	}
+}
--- UtilDemo/UtilDemo/Security/src/test/java/com/helloweb/utildemo/networking/HttpUtilTest.java
+++ UtilDemo/UtilDemo/Security/src/test/java/com/helloweb/utildemo/networking/HttpUtilTest.java
@@ -1,25 +1,44 @@
 package com.helloweb.utildemo.networking;
 
-import java.io.IOException;
-
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class HttpUtilTest {
 
-	@Test
-	public void testGet() throws IOException {
+	private final String url = "http://localhost:10000/";
+	private final int port = 10000;
+	private HttpServerMock mock;
 
-		String expected = "# JavaDemo\nJava demo\n";
-		String url = "https://raw.githubusercontent.com/HenryHYH/JavaDemo/master/README.md";
-		String message = HttpUtil.get(url);
+	@Before
+	public void setUp() throws Exception {
+		mock = new HttpServerMock(port);
+	}
 
-		Assert.assertEquals(expected, message);
+	@After
+	public void tearDown() throws Exception {
+		if (null != mock) {
+			mock.stop();
+		}
 	}
 
 	@Test
-	public void testPost() {
-		Assert.assertTrue(true);
+	public void testGet() throws Exception {
+
+		String expected = "Hello world!!!";
+		mock.start(expected, "text/plain");
+
+		Assert.assertEquals(expected, HttpUtil.get(url));
+	}
+
+	@Test
+	public void testPost() throws Exception {
+
+		String expected = "Hello world!!!";
+		mock.start(expected, "text/plain");
+
+		Assert.assertEquals(expected, HttpUtil.post(url, ""));
 	}
 
 }

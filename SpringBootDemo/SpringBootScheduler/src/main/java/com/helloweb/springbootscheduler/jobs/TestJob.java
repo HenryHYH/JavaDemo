3c464f8d7610a49edad2cc6692f287e226c74270
Henry <henry.hyh@gmail.com>
2017-08-03 15:40:56 +0800

    SpringBootDemo，测试定时作业中作业是否互斥运行

--- SpringBootDemo/SpringBootScheduler/src/main/java/com/helloweb/springbootscheduler/jobs/TestJob.java
+++ SpringBootDemo/SpringBootScheduler/src/main/java/com/helloweb/springbootscheduler/jobs/TestJob.java
@@ -4,28 +4,21 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
-// @Component
-public class SchedulerJob {
+@Component
+public class TestJob {
 
-	@Scheduled(fixedDelay = 2000) // 上一次执行完毕时间点后2秒再次执行
+	@Scheduled(fixedRate = 1000) // 上一次开始实行时间点后2秒再次执行
 	public void fixedDelayJob() throws Exception {
-		Thread.sleep(1000);
-		show("FixedDelayJob");
-	}
-
-	@Scheduled(fixedRate = 2000) // 上一次开始实行时间点后2秒再次执行
-	public void fixedRateJbo() {
-		show("FixedRateJob");
-	}
-
-	@Scheduled(cron = "0/5 * * * * ?")
-	public void cornJob() {
-		show("CornJob");
+		show("JobStart");
+		Thread.sleep(5000);
+		show("JobFinish");
 	}
 
 	private void show(String key) {
 		String str = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"));
 		System.out.println(String.format("%s\t: %s", key, str));
 	}
+
 }

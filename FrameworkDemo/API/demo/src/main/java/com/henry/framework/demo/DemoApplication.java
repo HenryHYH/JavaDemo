6fcc3cdf0caf9af6d6f07ef78777fb54d2ffee86
胡永晖 <huyonghui@gaohangip.com>
2020-04-12 14:25:41 +0800

    Framework，调整为Cloud，nacos config也改为cloud

--- FrameworkDemo/API/demo/src/main/java/com/henry/framework/demo/DemoApplication.java
+++ FrameworkDemo/API/demo/src/main/java/com/henry/framework/demo/DemoApplication.java
@@ -1,12 +1,9 @@
 package com.henry.framework.demo;
 
-import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@NacosPropertySource(dataId = "Framework", autoRefreshed = true)
 public class DemoApplication {
 
     public static void main(String[] args) {
--- FrameworkDemo/API/demo/src/main/java/com/henry/framework/demo/rest/ConfigController.java
+++ FrameworkDemo/API/demo/src/main/java/com/henry/framework/demo/rest/ConfigController.java
@@ -1,21 +1,22 @@
 package com.henry.framework.demo.rest;
 
-import com.alibaba.nacos.api.config.annotation.NacosValue;
-
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/config")
+@RefreshScope
 public class ConfigController {
 
-    @NacosValue(value = "${useLocalCache:false}", autoRefreshed = true)
-    private boolean userLocalCache;
+    @Value("${useLocalCache:false}")
+    private boolean useLocalCache;
 
     @GetMapping(value = "/get")
     public boolean get() {
-        return userLocalCache;
+        return useLocalCache;
     }
 
 }

2c6681276c64d2a5a69229145d106e8e65a9b859
胡永晖 <huyonghui@gaohangip.com>
2020-04-12 14:16:03 +0800

    Framework，添加响应时间返回值

--- FrameworkDemo/API/demo/src/main/java/com/henry/framework/demo/dto/result/ResultData.java
+++ FrameworkDemo/API/demo/src/main/java/com/henry/framework/demo/dto/result/ResultData.java
@@ -1,5 +1,8 @@
 package com.henry.framework.demo.dto.result;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import lombok.Getter;
 
 public class ResultData<T> {
@@ -9,6 +12,9 @@ public class ResultData<T> {
 
     @Getter
     private String message;
+
+    @Getter
+    private String responseAt;
 
     @Getter
     private T data;
@@ -21,6 +27,7 @@ public class ResultData<T> {
         this.code = resultCode.getCode();
         this.message = resultCode.getMessage();
         this.data = data;
+        this.responseAt = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"));
     }
 
 }
